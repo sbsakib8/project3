87c6d5652928446a9ce73e643e7b52f854540798
sakib <sbsakibsarkar8@gmail.com>
2024-10-02 22:46:25 +0600

    add

--- index.js
+++ index.js
@@ -59,3 +59,33 @@ tabs.forEach(tap =>{
     })
 })
 // ++++++++++++++++menu section end+++++++++++++++++++
+// swiper js
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+  spaceBetween: 100,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction:false
+  },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true
+    },
+    grabCursor:true,
+     // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 3,
+    }
+  } 
+  });
